2b437f33061cd27b42225232c7bf39f51a053cdc
Márcio Diniz <marciodiniz.dev@gmail.com>
2025-05-29 17:08:04 -0300

    Updates

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
+const interface = document.querySelector("#interface");
+const addProd = document.querySelector("#add-prod");
 const delProdButtons = document.querySelectorAll('.del-prod');
 const clearProd = document.querySelector('#clear-prod');
+
+// add products
+function addNewProd() {
+    
+}
 
 function delOne(event) {
     const contentArea = event.target.closest('.content-area');
